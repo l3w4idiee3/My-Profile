9a846792e032a4206138939f76a4d4a2a1d5a558
l3w4idiee3 <lewisjassy43@gmail.com>
2023-06-25 09:22:05 +0300

    removed js removed js css

--- profile.js
+++ profile.js
@@ -1,8 +1,2 @@
-window.addEventListener('DOMContentLoaded', function() {
-    var welcomeBanner = document.querySelector('.welcome-banner');
-    
-    setTimeout(function() {
-      welcomeBanner.classList.add('hidden');
-    }, 3000); // Adjust the delay (in milliseconds) to control how long the banner is displayed
-  });
+
   
